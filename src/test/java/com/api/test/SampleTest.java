60438d10610b28d6fea1fafffa1282eaa0540ece
Manu <mgmanu1996@gmail.com>
2025-12-31 12:57:01 +0530

    Updated

--- src/test/java/com/api/test/SampleTest.java
+++ src/test/java/com/api/test/SampleTest.java
@@ -14,9 +14,13 @@ public class SampleTest {
     @Test
     public void login(){
 //        RequestSpecification requestSpecification = ;
-        Response response = given()
-                .baseUri("http://64.227.160.186:8080/").contentType(ContentType.JSON)
-                .body("{ \"userName\": \"Manu MG\", \"password\": \"Manu@123\" }").post("/api/auth/login");
+//        Response response = given()
+//                .baseUri("http://64.227.160.186:8080/").contentType(ContentType.JSON)
+//                .body("{ \"userName\": \"Manu MG\", \"password\": \"Manu@123\" }").post("/api/auth/login");
+//        System.out.println(response.asPrettyString());
+
+        RequestSpecification requestSpecification = given().baseUri("https://petstore.swagger.io/v2");
+        Response response = requestSpecification.get("/store/inventory");
         System.out.println(response.asPrettyString());
 
 
